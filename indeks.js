000073e8b2b170a5c827dce8e17c0545d7b36390
Mateusz Zerebiec <43859749+ZerebiecMateusz@users.noreply.github.com>
2022-08-18 01:09:47 +0200

    working good

--- indeks.js
+++ indeks.js
@@ -1,4 +1,15 @@
 const btn = document.querySelector("button");
+let number = 1;
 btn.addEventListener('click', function () {
     console.log("click");
+    // let number = 1;
+    const newLiElement = document.createElement("li");
+    newLiElement.textContent = number;
+    document.body.appendChild(newLiElement);
+    const list = document.querySelector("ul");
+    list.appendChild(newLiElement);
+    number += 2;
+    if (number % 3 == 0) {
+        newLiElement.classList.add("big");
+    }
 })
